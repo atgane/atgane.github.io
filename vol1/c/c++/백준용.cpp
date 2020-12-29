0466305c09bf59a1d7cfc028bbb89b6b5ed826ba
atgane <hyper2012@naver.com>
2020-12-29 17:37:34 +0900

    20-12-29

--- vol1/c/c++/백준용.cpp
+++ vol1/c/c++/백준용.cpp
@@ -1,27 +1,6 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+#include<iostream>
 
-
-int main(){
-
-    vector<int> v;
-
-    v.push_back(10);
-    v.push_back(20);
-    v.push_back(30); 
-    v.push_back(40);
-    vector<int>::iterator iter=v.begin(); 
-    v.push_back(50);
- // 벡터 반복자 시작지점
-
-    cout << iter[3] << endl;  // 임의접근
-
-
-    // 반복
-    for (iter = v.begin(); iter != v.end(); ++iter){
-        cout << *iter << endl;
-    }
-
-    return 0;
+int main()
+{
+    std::cout << -3 % 7;
 }
--- vol1/c/c++/백준용1.cpp
+++ vol1/c/c++/백준용1.cpp
@@ -1,74 +1,30 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
 using namespace std;
-int n;
-vector<int> arr;
-vector<int>::iterator it_arr;
-vector<int> rep;
-int op[4] = {0};
+uint64_t m[101] = {0, 1, 1, 1, 2, 2, 3};
+uint64_t num;
 
-int bi_calc(int _x, int _y, int _a)
+uint64_t fib_seq(uint64_t n)
 {
-    if(_a == 0)
+    if(n <= 6)
     {
-        return _x + _y;
+        return m[n];
     }
-    else if(_a == 1)
+    else if(m[n] != 0)
     {
-        return _x - _y;
+        return m[n];
     }
-    else if(_a == 2)
-    {
-        return _x * _y;
-    }
-    else
-    {
-        return _x / _y;
-    }
-    
-}
-
-void calc(int _n, int v)
-{
-    if(_n == n - 1)
-    {
-        rep.push_back(v);
-    }
-    else
-    {
-        int ret;
-        for(int i = 0; i < 4; ++i)
-        {
-            if(op[i] >= 1)
-            {
-                op[i] -= 1;
-                ret = bi_calc(v, it_arr[_n + 1], i);
-                calc(_n + 1, ret);
-                op[i] += 1;
-            }
-        }
-    }
+    m[n] = fib_seq(n - 1) + fib_seq(n - 5);
+    return m[n];
 }
 
 int main()
 {
-    cin >> n;
-    for(int i = 0; i < n; ++i)
+    cin >> num;
+    for(int i = 0; i < num; ++i)
     {
-        int e;
-        cin >> e;
-        arr.push_back(e);
+        int n;
+        cin >> n;
+        cout << fib_seq(n) << endl;
     }
-    for(int i = 0; i < 4; ++i)
-    {
-        cin >> op[i];
-    }
-    it_arr = arr.begin();
-    calc(0, it_arr[0]);
-    int min = *min_element(rep.begin(), rep.end());
-    int max = *max_element(rep.begin(), rep.end());
-    cout << max << endl;
-    cout << min << endl;
     return 0;
 }
