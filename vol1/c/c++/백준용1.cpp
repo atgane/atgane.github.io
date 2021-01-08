4285da932068ea18d3b9f3664e5d6862f32087bc
atgane <hyper2012@naver.com>
2021-01-09 04:45:17 +0900

    21-01-09-01

--- vol1/c/c++/백준용1.cpp
+++ vol1/c/c++/백준용1.cpp
@@ -1,35 +1,12 @@
 #include<iostream>
-int max(int a, int b)
+int main()
 {
-    if(a > b)
-    {return a;}
-    else
-    {return b;}
-}
-int main(void)
-{
-    int n, memo[10001] = {0};
+    int n, m, cnt, data[1000];
     std::cin >> n;
-    int ram1, ram2;
-    for(int i = 1; i <= n; ++i)
+    std::cin >> m;
+    for(int i = 0; i < n; ++i)
     {
-        if(i == 1)
-        {
-            std::cin >> ram2;
-            memo[i] = ram2;
-        }
-        else if(i == 2)
-        {
-            ram1 = ram2;
-            std::cin >> ram2;
-            memo[i] = ram1 + ram2;
-        }
-        else
-        {
-            ram1 = ram2;
-            std::cin >> ram2;
-            memo[i] = max(memo[i - 1], max(memo[i - 2] + ram2, memo[i - 3] + ram1 + ram2));
-        }
+        std::cin >> data[i];
     }
-    std::cout << memo[n];
+
 }
