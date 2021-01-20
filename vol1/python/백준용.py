3de72415920ca0cb7f01ca2c8f691c735686d4c9
atgane <hyper2012@naver.com>
2021-01-21 01:44:07 +0900

    21-01-21-04

--- vol1/python/백준용.py
+++ vol1/python/백준용.py
@@ -1,16 +1,8 @@
 import sys
-n = int(sys.stdin.readline())
-for _ in range(n):
-    k = int(sys.stdin.readline())
-    vol_dict ={}
-    for _ in range(k):
-        s = sys.stdin.readline().split()
-        if s[1] not in vol_dict.keys():
-            vol_dict[s[1]] = 0
-        vol_dict[s[1]] += 1
-    vol_list = list(vol_dict.values())
-    vol_len = len(vol_list)
-    ret = 1
-    for i in range(vol_len):
-        ret *= vol_list[i] + 1
-    print(ret - 1)
+import math
+n = str(math.factorial(int(sys.stdin.readline())))
+l = len(n)
+for i in range(l):
+    if n[-(i + 1)] != '0':
+        print(i)
+        break
