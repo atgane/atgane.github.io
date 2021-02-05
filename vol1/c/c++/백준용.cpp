139e2df8fb65cf16525cf4af6393a9d9e987af8d
atgane <hyper2012@naver.com>
2021-02-06 02:43:06 +0900

    21-02-06-03

--- vol1/c/c++/백준용.cpp
+++ vol1/c/c++/백준용.cpp
@@ -1,28 +1,22 @@
-#include<deque>
 #include<iostream>
+
+int mul(int _A, int _B, int _C)
+{
+    if(_B == 1)
+    {return _A % C;}
+    else if(_B % 2 == 0)
+    {
+        return mul(_A, _B / 2, _C) * mul(_A, _B / 2, _C) % _C;
+    }
+    else
+    {
+        return mul(_A, _B / 2, _C) * mul(_A, _B / 2, _C) * 2 % _C;
+    }
+}
 
 int main()
 {
-    int n, flag = 1;
-    std::deque<int> deq;
-    std::cin >> n;
-    for(int i = 1; i <= n; ++i)
-    {
-        deq.push_back(i);
-    }
-    while(deq.size() != 1)
-    {
-        if(flag % 2 == 1)
-        {
-            deq.pop_front();
-        }
-        else
-        {
-            int tmp = deq.front();
-            deq.push_back(tmp);
-            deq.pop_front();
-        }   
-        flag += 1;
-    }
-    std::cout << deq.front();
+    int A, B, C;
+    std::cin >> A >> B >> C;
+    std::cout << mul(A, B, C);
 }
