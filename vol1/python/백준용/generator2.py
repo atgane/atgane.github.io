16f0f506ed97824180039fa23c615e637b04b017
atgane <hyper2012@naver.com>
2021-02-07 04:16:11 +0900

    21-02-07-01

--- vol1/python/백준용/generator2.py
+++ vol1/python/백준용/generator2.py
@@ -2,12 +2,3 @@ import random
 n = random.randint(2, 10)
 q = random.randint(2, 10)
 print(n, q)
-for i in range(n - 1):
-    print(i + 1)
-    
-for i in range(n + q - 1):
-    t = random.randint(0, 1)
-    if t == 1:
-        print(t, random.randint(1, n), random.randint(1, n))
-    else:
-        print(t, random.randint(1, n))
