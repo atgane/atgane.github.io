ccf839e90d943af85a09ede87332c9946a6b89e4
atgane <hyper2012@naver.com>
2021-02-17 18:06:32 +0900

    21-02-17-03

--- vol1/c/c++/백준용.cpp
+++ vol1/c/c++/백준용.cpp
@@ -3,7 +3,7 @@
 int mul(int _A, int _B, int _C)
 {
     if(_B == 1)
-    {return _A % C;}
+    {return _A % _C;}
     else if(_B % 2 == 0)
     {
         return mul(_A, _B / 2, _C) * mul(_A, _B / 2, _C) % _C;
