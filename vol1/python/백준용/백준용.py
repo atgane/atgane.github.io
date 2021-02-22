f7ab88a8e0debf7631a65dd7929883b9f07f217a
atgane <hyper2012@naver.com>
2021-02-22 20:43:20 +0900

    21-02-22-01

--- vol1/python/백준용/백준용.py
+++ vol1/python/백준용/백준용.py
@@ -1,9 +1,35 @@
 import sys
+import heapq
+
 ssr = sys.stdin.readline
 V, E = list(map(int, ssr().split()))
 K = int(ssr())
-graph = [list(map(int, ssr().split())) for _ in range(E)]
+graph = {}
+for i in range(V):
+    graph[i + 1] = {}
+for i in range(E):
+    tmp = list(map(int, ssr().split()))
+    graph[tmp[0]][tmp[1]] = tmp[2]
 print(graph)
 
+def dijkstra(graph, start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    queue = []
+    heapq.heappush(queue, [distances[start], start])
 
+    while queue:
+        current_distance, current_destination = heapq.heappop(queue)
 
+        if distances[current_destination] < current_distance:
+            continue
+        
+        for new_destination, new_distance in graph[current_destination].items():
+            distance = current_distance + new_distance 
+            if distance < distances[new_destination]: 
+                distances[new_destination] = distance
+                heapq.heappush(queue, [distance, new_destination]) 
+        
+    return distances
+
+print(dijkstra(graph, K))
--- vol1/python/백준용/백준용1.py
+++ vol1/python/백준용/백준용1.py
@@ -1,3 +1,5 @@
-import heapq
-
-queue = []
+import sys
+a = {}
+a[1] = {2: 3}
+a[1][3] = 5
+print(a)
